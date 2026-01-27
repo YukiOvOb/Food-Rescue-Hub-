500cf2fba33f3681ac99cd2a8433f821ed98532e
Pheng Boon <tanphengboon@outlook.sg>
2026-01-28 02:19:47 +0800

    fixed import java.sql.*  libraries

--- backend/src/main/java/com/frh/backend/controller/HelloController.java
+++ backend/src/main/java/com/frh/backend/controller/HelloController.java
@@ -4,10 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Map;
 
 @RestController
